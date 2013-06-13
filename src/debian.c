2ea000d5ac7383b82df824d5ed9ddb77b8328f2e
fuzzgun <bob@sluggish.dyndns.org>
2013-06-13 16:32:33 +0100

    BSD license

--- src/debian.c
+++ src/debian.c
@@ -1797,6 +1797,39 @@ static void save_license_mit(char * filename)
 	fclose(fp);
 }
 
+/* saves a LICENSE file for BSD */
+static void save_license_bsd(char * filename)
+{
+	FILE * fp;
+	time_t rawtime;
+	struct tm * timeinfo;
+	int year;
+	char email_address[BLOCK_SIZE];
+
+	get_setting("email",email_address);
+
+	/* get the current year */
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	year = timeinfo->tm_year + 1900;
+
+	fp = fopen(filename,"w");
+	if (!fp) return;
+
+	fprintf(fp,"Copyright (c) %d, %s\n",year,email_address);
+	fprintf(fp,"All rights reserved.\n\n");
+
+	fprintf(fp,"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n");
+
+	fprintf(fp,"    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n");
+	fprintf(fp,"    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n");
+	fprintf(fp,"    Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n");
+
+	fprintf(fp,"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n");
+
+	fclose(fp);
+}
+
 /* saves a file called LICENSE */
 static void save_license(char * directory)
 {
