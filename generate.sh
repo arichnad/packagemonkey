39b80300eb794b9c166ece2d0ce36de685050c2d
fuzzgun <bob@sluggish.dyndns.org>
2013-06-26 12:15:47 +0100

    Generate script

---
+++ generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Generates packaging for packagemonkey
+
+rm -rf debian rpmpackage archpackage puppypackage
+rm -f rpm.sh debian.sh arch.sh puppy.sh Makefile
+
+./packagemonkey --cmd --dir "/home/motters/develop/packagemonkey" -l "gpl3" -e "Bob Mottram (4096 bits) <bob@robotics.uk.to>" --brief "Tool to make packaging easier" --desc "Creating installable packages for Linux distros has always been a bit of a black art.  There is a lot of documentation out there and it's often voluminous and not especially easy to interpret.  When doing packaging by hand there are also many ways to go wrong.  Packagemonkey is intended to make packaging simpler, and enable the creation of multiple different types of package." --homepage "https://github.com/fuzzgun/packagemonkey" --section "utils" --version "0.10" --categories "Utility/ConsoleOnly" --compile "-lz" --dependsdeb "build-essential, lintian"
