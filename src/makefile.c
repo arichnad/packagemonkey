e96114060f5adea087627d1e39005a3b5fa78510
Bob Mottram <bob@robotics.uk.to>
2013-07-23 21:17:07 +0100

    /usr/local or /usr

--- src/makefile.c
+++ src/makefile.c
@@ -454,10 +454,28 @@ static void save_makefile_install_scripts(char * filename,
 	sprintf(str, "echo '#!/bin/sh' > %s", runscript);
 	add_makefile_entry_to_file(filename, section, str);
 
+	/* if installed to the local directory */
+	sprintf(str, "echo 'if [ -d /usr/local/share/${APP} ]; then' >> %s",
+			runscript);
+	add_makefile_entry_to_file(filename, section, str);
+
 	/* move to the project directory */
-	sprintf(str, "echo 'cd ${PREFIX}/share/%s' >> %s",
+	sprintf(str, "echo '  cd /usr/local/share/${APP}' >> %s",
+			runscript);
+	add_makefile_entry_to_file(filename, section, str);
+
+	/* else */
+	sprintf(str, "echo 'else' >> %s", runscript);
+	add_makefile_entry_to_file(filename, section, str);
+	
+	sprintf(str, "echo '  cd /usr/share/%s' >> %s",
 			project_name, runscript);
 	add_makefile_entry_to_file(filename, section, str);
+
+	/* endif */
+	sprintf(str, "echo 'fi' >> %s", runscript);
+	add_makefile_entry_to_file(filename, section, str);
+
 
 	/* run the main script */
 	if (is_script_language(project_type) != 0) {
